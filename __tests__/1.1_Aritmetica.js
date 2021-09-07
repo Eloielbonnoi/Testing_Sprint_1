a39b87dec5c467bf3344495167f442b88ab1c39a
Eloi Isern <nuntxaku@gmail.com>
2021-09-07 12:09:40 +0200

    Node_modules a .gitignore

---
+++ __tests__/1.1_Aritmetica.js
@@ -0,0 +1,7 @@
+//Creu un arxiu amb les funcions sumar, restar, multiplicar i dividir 2 o més operands. 
+//Testi la correcta execució d'aquestes funcions.
+
+function sum(a, b) {
+    return a + b;
+  }
+  module.exports = sum;
