40f9b8c12caadac925060531154d24940011c27c
Eloi Isern <nuntxaku@gmail.com>
2021-10-16 11:48:05 +0200

    ordenació carpeta i arxius

--- app/async_await_1.js
+++ app/async_await_1.js
@@ -28,7 +28,7 @@ let salaries = [{
 }];
 
 const getEmpleado = (id) => {
-    //Test que si no hi ha id et retorni9 l'error correcte
+    //Test que si no hi ha id et retorni l'error correcte
     //Test que la id és un número
     //Test que la id no conté lletres
 
@@ -62,28 +62,3 @@ const getSalaries = (objEmployee) => {
 };
 
 
-//NIVELL 2
-//Crea una funció asíncrona que anomeni a una altra que retorni una Promise que efectuï la seva resolve amb una demora de 2 segons.
-
-const dades = false;
-const getDades = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (!dades) {
-                const individuu = {
-                    id: 1,
-                    nom: 'Eloi',
-                    professio: 'Músic'
-                }
-                resolve(individuu);
-            } else {
-                reject(new Error('Hi ha alguna cosa que no rutlla'));
-            }
-        }, 2000);
-    })
-}
-async function escriuDades() {
-    const dadesPerEscriure = await getDades();
-    console.log(dadesPerEscriure);
-}
-escriuDades().catch(err => console.log(err.message));
